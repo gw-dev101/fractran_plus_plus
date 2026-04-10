0cf02cca583979fef36a68e3d75c3c6efed5fc3a
Gwenole Roger <gwenole.roger.dev@gmail.com>
2026-04-10 08:12:52 +0200

    Add tests for lexer.Tokenize to validate tokenization with comments

--- internal/lexer/lexer_test.go
+++ internal/lexer/lexer_test.go
@@ -3,5 +3,30 @@ package lexer
 import "testing"
 
 func TestTokenize(t *testing.T) {
-	t.Skip("TODO: add lexer tests")
+	//expect comments to be ignored
+	//check that the lexed of "1/2, 3/1, 4/5" and "1/2,3/1this is a comment,4/5" are the same
+	input1 := "1/2, 3/1, 4/5"
+	input2 := "1/2,3/1this is a comment,4/5"
+
+	lexer := New()
+
+	tokens1, err1 := lexer.Tokenize(input1)
+	if err1 != nil {
+		t.Fatalf("unexpected error: %v", err1)
+	}
+
+	tokens2, err2 := lexer.Tokenize(input2)
+	if err2 != nil {
+		t.Fatalf("unexpected error: %v", err2)
+	}
+
+	if len(tokens1) != len(tokens2) {
+		t.Fatalf("expected same number of tokens, got %d and %d", len(tokens1), len(tokens2))
+	}
+
+	for i := range tokens1 {
+		if tokens1[i] != tokens2[i] {
+			t.Errorf("expected token %d to be the same, got %v and %v", i, tokens1[i], tokens2[i])
+		}
+	}
 }
