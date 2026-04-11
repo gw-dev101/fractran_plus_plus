4b6a18bb00c252bba45e4362e498ddef241cf400
gwenole roger <gwenole.roger.dev@gmail.com>
2026-04-11 17:23:40 +0000

    go fmt

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
+
 //import everything we used to test the compiler, interpreter, lexer, and parser
 import (
-    "github.com/gw-dev101/fractran_plus_plus/internal/compiler"
-    "github.com/gw-dev101/fractran_plus_plus/internal/interpreter"
-    "github.com/gw-dev101/fractran_plus_plus/internal/lexer"
-    "github.com/gw-dev101/fractran_plus_plus/internal/parser"
+	"github.com/gw-dev101/fractran_plus_plus/internal/compiler"
+	"github.com/gw-dev101/fractran_plus_plus/internal/interpreter"
+	"github.com/gw-dev101/fractran_plus_plus/internal/lexer"
+	"github.com/gw-dev101/fractran_plus_plus/internal/parser"
 )
 
 func main() {
-    //try to run some tests
-    compiler.TestCompile(nil)
-    interpreter.TestExecute(nil)
-    lexer.TestTokenize(nil)
-    parser.TestParse(nil)   
+	//try to run some tests
+	compiler.TestCompile(nil)
+	interpreter.TestExecute(nil)
+	lexer.TestTokenize(nil)
+	parser.TestParse(nil)
 }
