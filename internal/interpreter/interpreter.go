0fcf752dc1f6934ceb887048234a568f9247d58e
gwenole roger <gwenole.roger.dev@gmail.com>
2026-04-12 22:16:01 +0000

    Implement Execute and Step methods in Interpreter for FRACTRAN++ program execution

--- internal/interpreter/interpreter.go
+++ internal/interpreter/interpreter.go
@@ -1,14 +1,11 @@
 package interpreter
 
 import (
-	"errors"
 	"fmt"
+	"math/big"
 
 	"github.com/gw-dev101/fractran_plus_plus/internal/ast"
 )
-
-// ErrNotImplemented is returned by the stub interpreter until execution exists.
-var ErrNotImplemented = errors.New("interpreter not implemented")
 
 // Result describes the outcome of interpreting a program.
 type Result struct {
@@ -28,7 +25,36 @@ func New() *Interpreter {
 	return &Interpreter{}
 }
 
-// Execute is a stub placeholder for the real interpreter implementation.
-func (i *Interpreter) Execute(program *ast.Program) (Result, error) {
-	return Result{}, ErrNotImplemented
+func (i *Interpreter) Execute(program *ast.Program, input *big.Int) (Result, error) {
+	state := new(big.Int).Set(input)
+	steps := 0
+	for {
+		newState, changed, err := i.Step(program, state)
+		if err != nil {
+			return Result{}, err
+		}
+		if !changed {
+			return Result{Steps: steps, Halted: true}, nil
+		}
+		state.Set(newState)
+		steps++
+	}
+
+}
+func (i *Interpreter) Step(program *ast.Program, state *big.Int) (*big.Int, bool, error) {
+	for _, stmt := range program.Statements {
+		switch s := stmt.(type) {
+		case ast.Fraction:
+			var tmp big.Int
+			tmp.Mod(state, s.Denominator)
+			if tmp.Sign() == 0 {
+				newState := new(big.Int).Mul(state, s.Numerator)
+				newState.Div(newState, s.Denominator)
+				return newState, true, nil
+			}
+		default:
+			return nil, false, fmt.Errorf("unknown statement type: %T", stmt)
+		}
+	}
+	return state, false, nil
 }
